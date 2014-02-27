227ade7bdc9bd35f69627fac8dd16662dc184a11
Gustavo Castro <gustavomdcl@gmail.com>
2014-02-27 15:57:45 -0300

    Grunt, admin de tarefas, SVG introducao e compact js

---
+++ _ProjetoInicial/Avançado/src/Gruntfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+module.exports = function( grunt ) {
+
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+
+    // configuracoes das tarefas
+
+    watch: {
+      css: {
+        files: '../assets/sass/**/*' ,
+        tasks: [ 'compass' ]
+      },
+      js: {
+        files: '../assets/js/**/*',
+        tasks: [ 'uglify' ]
+      }
+    },
+    // Compile scss
+    compass: {
+      dist: {
+        options: {
+          /*force: true,
+          config: '../assets/config.rb',
+          outputStyle: 'compressed'*/
+          sassDir: '../assets/sass',
+          cssDir: '../assets/css',
+          imagesDir: '../assets/img',
+          boring: false,
+          outputStyle: 'compressed',
+        }
+      }
+    },
+
+    // Concat and minify javascripts
+    uglify: {
+      options: {
+        mangle: false
+      },
+      dist: {
+        files: {
+          '../assets/min/jquery.suiting.min.js': [
+            '../assets/js/jquery.suiting.js'
+          ],
+          '../assets/min/jquery.mobile.min.js': [
+            '../assets/js/jquery.mobile.js'
+          ]
+        }
+      }
+    },
+
+  });
+
+  // registrando tarefas
+  grunt.registerTask( 'default', [ 'watch' ] );
+
+};
--- _ProjetoInicial/Avançado/template/script.php
+++ _ProjetoInicial/Avançado/template/script.php
@@ -1,13 +1,13 @@
 	<!-- javascript
 	======================================================== -->
 	<!-- modernizr modernizr.com -->
-	<script src="assets/js/modernizr.js"></script>
+	<script src="assets/min/modernizr.min.js"></script>
 	<!-- jquery jquery.com -->
-	<script src="assets/js/jquery.js?v=1.11.0"></script>
+	<script src="assets/min/jquery.min.js?v=1.11.0"></script>
 	<!-- suiting -->
-	<script src="assets/js/jquery.suiting.js"></script>
+	<script src="assets/min/jquery.suiting.min.js"></script><!-- GRUNT http://blog.henriquesilverio.com/javascript-e-jquery/grunt-js-automatize-tarefas-e-otimize-o-seu-workflow/ -->
 	<!-- mobile -->
-	<script src="assets/js/jquery.mobile.js"></script>
+	<script src="assets/min/jquery.mobile.min.js"></script><!-- GRUNT http://blog.henriquesilverio.com/javascript-e-jquery/grunt-js-automatize-tarefas-e-otimize-o-seu-workflow/ -->
 	<!-- analytics -->
 	<script type="text/javascript">
 	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
