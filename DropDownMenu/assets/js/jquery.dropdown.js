614a2aac1db02e49d3081b2d43ada36f9f0d5971
Gustavo Lima <gustavomdcl@gmail.com>
2014-05-10 11:52:44 -0300

    Arrumou dropdown

--- DropDownMenu/assets/js/jquery.dropdown.js
+++ DropDownMenu/assets/js/jquery.dropdown.js
@@ -1,9 +1,9 @@
 /*! jQuery Dropdown v1.00.0 | (c) 2014, 2014 | */
 
-$('.nav li').mouseover(function () {
+$('.nav li').mouseenter(function () {
 	$(this).children('.nav').css('display','block');
 });
 
-$('.nav li').mouseout(function () {
+$('.nav li').mouseleave(function () {
 	$(this).children('.nav').css('display','none');
 });
